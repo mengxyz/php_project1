1d207438d5294c4e0de74f297881d853b0940fe8
mengxyz <meng348@gmail.com>
2019-10-23 16:21:57 +0700

    add font

--- eportfolio_bt4/cdn.php
+++ eportfolio_bt4/cdn.php
@@ -2,11 +2,4 @@
 <link rel="stylesheet" href="./css/bootstrap.min.css">
 <script src="./js/bootstrap.min.js"></script>
 <link rel="stylesheet" href="./css/all.css">
-<link href="https://fonts.googleapis.com/css?family=Bai+Jamjuree&display=swap" rel="stylesheet"> 
-<style>
-body,
-html {
-    height: 80vh;
-    font-family: 'Bai Jamjuree', sans-serif;
-}
-</style>
+<link rel="stylesheet" href="./css/base.css">
